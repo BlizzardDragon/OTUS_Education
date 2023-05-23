5f295bd21e64087b25e1842723c673d47b0db1a1
BlizzardDragon <blizzarddragon@list.ru>
2023-05-24 00:34:23 +0300

    added DeathTrigger

--- Assets/Homeworks/Homework_1/Scripts/Enemy/EnemyCollider.cs
+++ Assets/Homeworks/Homework_1/Scripts/Enemy/EnemyCollider.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class EnemyCollider : MonoBehaviour
 {
+    [SerializeField] private Enemy _enemy;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<DeathTrigger>())
+        {
+            Destroy(_enemy.gameObject);
+        }
+    }
 }
--- Assets/Homeworks/Homework_1/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Homeworks/Homework_1/Scripts/Enemy/EnemySpawner.cs
@@ -2,26 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour, IGameStartListener, IGameFinishListener, IInitListener
+public class EnemySpawner : MonoBehaviour, IGameUpdateListener, IGameStartListener, IGameFinishListener, IInitListener
 {
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private Transform[] _jumpTargets;
+    private const float TIME_MAX_DIFFICULTY = 300;
+    private float _chanceThreshold;
+    private float _time;
 
 
     public void OnStartGame()
     {
-        ServiceLocator.GetService<RoadSpawner>().OnSpawnRoad += SpawnEnemy;
+        ServiceLocator.GetService<RoadSpawner>().OnSpawnRoad += CalculateSpawnEnemy;
     }
 
     public void OnFinishGame()
     {
-        ServiceLocator.GetService<RoadSpawner>().OnSpawnRoad -= SpawnEnemy;
+        ServiceLocator.GetService<RoadSpawner>().OnSpawnRoad -= CalculateSpawnEnemy;
     }
 
-    public void SpawnEnemy(Vector3 roadSpawnPosition)
+    public void OnUpdate(float deltaTime)
     {
-        int randomJumpTarget = Random.Range(0, _jumpTargets.Length);
-        Vector3 spawnPosition = new Vector3(_jumpTargets[randomJumpTarget].position.x, roadSpawnPosition.y, roadSpawnPosition.z);
+        _time += deltaTime;
+    }
+
+    public void CalculateSpawnEnemy(Vector3 roadSpawnPosition)
+    {
+        _chanceThreshold = (_time / TIME_MAX_DIFFICULTY) * 0.8f;
+        float randomValue = Random.value;
+        int enemyCount = 0;
+
+        if (randomValue > _chanceThreshold)
+        {
+            int index = Random.Range(0, _jumpTargets.Length);
+            SpawnEnemy(index, roadSpawnPosition);
+        }
+        else
+        {
+            while (enemyCount < 2)
+            {
+                int index = Random.Range(0, _jumpTargets.Length);
+                SpawnEnemy(index, roadSpawnPosition);
+                enemyCount++;
+            }
+        }
+
+    }
+
+    private void SpawnEnemy(int index, Vector3 roadSpawnPosition)
+    {
+        Vector3 spawnPosition = new Vector3(_jumpTargets[index].position.x, roadSpawnPosition.y, roadSpawnPosition.z);
         Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity, transform);
     }
 }
