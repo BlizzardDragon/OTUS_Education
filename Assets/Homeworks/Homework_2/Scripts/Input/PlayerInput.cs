9b067655f0f2ca6fcdd61f43a8d5323d8837f55b
BlizzardDragon <blizzarddragon@list.ru>
2023-06-09 16:13:10 +0300

    refact PlayerInput

---
+++ Assets/Homeworks/Homework_2/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace ShootEmUp
+{
+    public sealed class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private MoveComponent _moveComponent;
+        [SerializeField] private CharacterController _characterController;
+        private float _horizontalDirection;
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _characterController._fireRequired = true;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                _horizontalDirection = -1;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                _horizontalDirection = 1;
+            }
+            else
+            {
+                _horizontalDirection = 0;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 direction = new Vector2(_horizontalDirection, 0) * Time.fixedDeltaTime;
+            _moveComponent.MoveByRigidbodyVelocity(direction);
+        }
+    }
+}
