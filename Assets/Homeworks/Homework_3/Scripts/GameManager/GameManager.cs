613b64223b8f3eb516e3aeae96d403874e5ece71
BlizzardDragon <blizzarddragon@list.ru>
2023-06-19 22:32:06 +0300

    added Odin

---
+++ Assets/Homeworks/Homework_3/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FrameworkUnity.Architecture.GameManagers;
+
+public class GameManager : BaseGameManager
+{
+    
+}
