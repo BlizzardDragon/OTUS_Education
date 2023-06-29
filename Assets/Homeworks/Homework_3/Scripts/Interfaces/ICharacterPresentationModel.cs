5950d9bf26fb604f6fc1a4b9f2992b26bcc75ff9
BlizzardDragon <blizzarddragon@list.ru>
2023-06-29 21:18:15 +0300

    fixed ICharacterPresentationModel

--- Assets/Homeworks/Homework_3/Scripts/Interfaces/ICharacterPresentationModel.cs
+++ Assets/Homeworks/Homework_3/Scripts/Interfaces/ICharacterPresentationModel.cs
@@ -6,32 +6,14 @@ using UnityEngine;
 
 namespace PresentationModel
 {
-    public class ICharacterPresentationModel : MonoBehaviour
+    public interface ICharacterPresentationModel
     {
-        internal string Name => _name;
-        private string _name;
-
         public Action<string, string, float> OnExperienceChanged { get; internal set; }
 
 
-        internal string GetDescription()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal Sprite GetIcon()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal string GetLevel()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal string GetName()
-        {
-            throw new NotImplementedException();
-        }
+        string GetDescription();
+        Sprite GetIcon();
+        string GetLevel();
+        string GetName();
     }
 }
