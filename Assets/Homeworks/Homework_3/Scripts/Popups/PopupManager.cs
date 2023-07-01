bd96ded24969b407d4d895fd12b251f7ac212ee5
BlizzardDragon <blizzarddragon@list.ru>
2023-07-01 15:42:14 +0300

    mini fix popup manager

--- Assets/Homeworks/Homework_3/Scripts/Popups/PopupManager.cs
+++ Assets/Homeworks/Homework_3/Scripts/Popups/PopupManager.cs
@@ -9,6 +9,7 @@ namespace PresentationModel
     public class PopupManager : MonoBehaviour
     {
         [SerializeField] private CharacterPopup _characterPopup;
+        // Пришлось изменить CharacterStat. Без этого ни как не получалось.
         [SerializeField] private CharacterStat[] _characterStats;
         [Space(15)]
         [ShowInInspector] private CharacterPopupPresentationModel _presentationModel;
