52d327307af75dc385de92cf0d01521bd46d25c2
BlizzardDragon <blizzarddragon@list.ru>
2023-07-08 23:03:18 +0300

    added HitComponent

---
+++ Assets/Homeworks/Homework_7/Scripts/Components/HitComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace OTUS_Education.Assets.Homeworks.Homework_7.Scripts.Components
+{
+    public struct HitComponent
+    {
+        public GameObject FirstCollide;        
+        public GameObject SecondCollide;        
+    }
+}
