7475fc7fcaeda778ecde0ae932f78c34e67d3f31
BlizzardDragon <blizzarddragon@list.ru>
2023-07-08 21:54:32 +0300

    added UnitViewComponent and TeamComponent

---
+++ Assets/Homeworks/Homework_7/Scripts/Components/TeamComponent.cs
@@ -0,0 +1,14 @@
+public enum Teams
+{
+    Null,
+    Blue,
+    Red
+}
+
+namespace OTUS_Education.Assets.Homeworks.Homework_7.Scripts.Components
+{
+    public struct TeamComponent
+    {
+        public Teams Team;
+    }
+}
